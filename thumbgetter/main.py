a8d23da9e6510991ecd28909a6bebd8e0d3807b2
Gustavo Nunes <gustavompn06@gmail.com>
2023-08-31 10:21:25 -0300

    Organização dos arquivos e atualização da rota de redirecionamento.
    Fim do projeto.

--- thumbgetter/main.py
+++ thumbgetter/main.py
@@ -4,16 +4,13 @@ from forms import UrlYoutube
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "c401f29fac31b7c2972f29d97a4f37c6" 
 
-url = ''
-img = ''
-
 @app.route('/', methods=["GET", "POST"])
 def homepage():
     form = UrlYoutube()
     if form.validate_on_submit():
         url = str(form.url.data)
         id = url.split('v=')[1]
-        #return redirect(f'https://i3.ytimg.com/vi/{id}/maxresdefault.jpg')
+        return redirect(f'https://i3.ytimg.com/vi/{id}/maxresdefault.jpg')
     return render_template("home.html", form=form)
 
 if __name__ == '__main__':
